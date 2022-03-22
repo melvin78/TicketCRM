f5346efdf0c3e9eea9da362c4e17971b8278bd86
melvin <melvin.mark@centrino.co.ke>
2022-03-22 08:38:53 +0300

    add agents dashboard to client list oidc

--- IdentityServerAspNetIdentity/Config.cs
+++ IdentityServerAspNetIdentity/Config.cs
@@ -12,30 +12,15 @@ namespace IdentityServerAspNetIdentity
     {
         public static IEnumerable<IdentityResource> GetIdentityResources()
         {
-            // var customProfile = new IdentityResource(
-            //     name: "custom.profile",
-            //     displayName: "Custom profile",
-            //     userClaims: new[] { "sacco_id","first_name","second_name","isCustomer" });
 
             return new List<IdentityResource>
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
-                // customProfile
+              
             };
         }
-        // public static IEnumerable<IdentityResource> IdentityResources =>
-        //     
-        //            new IdentityResource[]
-        //            {
-        //         new IdentityResources.OpenId(),
-        //         new IdentityResources.Profile(),
-        //         new IdentityResource(
-        //             name: "other_info",
-        //             userClaims: new[] { "sacco_id","first_name","second_name","isCustomer" },
-        //             displayName: "Your user identifier")
-        //         
-        //            };
+    
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
@@ -63,14 +48,7 @@ namespace IdentityServerAspNetIdentity
                     AllowedGrantTypes = GrantTypes.Code,
                     AllowAccessTokensViaBrowser = true,
                     AccessTokenLifetime = 120,
-                    // AllowOfflineAccess = true,
-                    // AccessTokenLifetime = 90, // 1.5 minutes
-                    // AbsoluteRefreshTokenLifetime = 0,
-                    // RefreshTokenUsage = TokenUsage.OneTimeOnly,
-                    // RefreshTokenExpiration = TokenExpiration.Sliding,
-                    // UpdateAccessTokenClaimsOnRefresh = true,
-                    // RequireConsent = false,
-
+                  
                
          
                     RedirectUris =           { "https://support-ui.webmelvin.me/account/callback" },
@@ -97,25 +75,19 @@ namespace IdentityServerAspNetIdentity
          
                 new Client
                 {
-                    ClientId = "js",
-                    ClientName = "Centrino Help Desk Support",
+                    ClientId = "agent_ui",
+                    ClientName = "Agent UI",
                     RequireClientSecret = false,
                     AllowedGrantTypes = GrantTypes.Code,
                     AllowAccessTokensViaBrowser = true,
                     AccessTokenLifetime = 120,
-                    // AllowOfflineAccess = true,
-                    // AccessTokenLifetime = 90, // 1.5 minutes
-                    // AbsoluteRefreshTokenLifetime = 0,
-                    // RefreshTokenUsage = TokenUsage.OneTimeOnly,
-                    // RefreshTokenExpiration = TokenExpiration.Sliding,
-                    // UpdateAccessTokenClaimsOnRefresh = true,
-                    // RequireConsent = false,
+                  
 
                
          
-                    RedirectUris =           { "http://localhost:7893/account/callback" },
-                    PostLogoutRedirectUris = { "http://localhost:7893" },
-                    AllowedCorsOrigins =     { "http://localhost:7893" },
+                    RedirectUris =           { "https://agents-portal.webmelvin.me/account/callback" },
+                    PostLogoutRedirectUris = { "https://agents-portal.webmelvin.me" },
+                    AllowedCorsOrigins =     { "https://agents-portal.webmelvin.me" },
 
                     AllowedScopes =
                     {
@@ -134,45 +106,7 @@ namespace IdentityServerAspNetIdentity
                     },
                 },
                 
-                new Client
-                {
-                ClientId = "jsagent",
-                ClientName = "VueJs JavaScript Agent",
-                RequireClientSecret = false,
-                AllowedGrantTypes = GrantTypes.Code,
-                AllowAccessTokensViaBrowser = true,
-                AccessTokenLifetime = 120,
-            
-                // AllowOfflineAccess = true,
-                // AccessTokenLifetime = 90, // 1.5 minutes
-                // AbsoluteRefreshTokenLifetime = 0,
-                // RefreshTokenUsage = TokenUsage.OneTimeOnly,
-                // RefreshTokenExpiration = TokenExpiration.Sliding,
-                // UpdateAccessTokenClaimsOnRefresh = true,
-                // RequireConsent = false,
-
-               
-         
-                RedirectUris =   { "http://localhost:7892/account/callback" },
-                PostLogoutRedirectUris = { "http://localhost:7892" },
-                AllowedCorsOrigins =  { "http://localhost:7892" },
-
-                AllowedScopes =
-                {
-                IdentityServerConstants.StandardScopes.OpenId,
-                IdentityServerConstants.StandardScopes.Profile,
-                // "custom.profile",
-                "second_name",
-                "first_name",
-                "sacco_id",
-                "api1",
-                "api2",
-                "enquiries.read",
-                "enquiries.write",
-                "enquiries"
-                        
-                },
-                }
+       
             };
     }
 }
